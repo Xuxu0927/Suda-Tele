ccd8a8aa81568336c75a6dd9b69861c063ce2496
Xuxu0927 <2889087473@qq.com>
2025-01-27 15:24:56 +0800

    1

--- docs/MATLAB/实验/设计1/sy1.m
+++
@@ -1,8 +0,0 @@
-function fc=sy1(x,y)
-d=sqrt(x^2+y^2);
-if d<=80
-    fc='可以与该目标节点通信';
-else
-    fc='不可以与该目标节点通信';
-end
-end
--- docs/微机应用（new）/电子日历/LED.h
+++
@@ -1,4 +0,0 @@
-#ifndef __LED_H_
-#define __LED_H_
-void LED(unsigned char x);
-#endif
